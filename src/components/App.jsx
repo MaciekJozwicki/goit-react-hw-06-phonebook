4a30249520e37aedf0854281307562e2a86c0d66
Maciek <maciekjozwicki@wp.pl>
2024-01-11 01:43:31 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,3 @@
-import { useEffect } from 'react';
-import { useState } from 'react';
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
